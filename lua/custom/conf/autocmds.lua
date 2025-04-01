f999fee6913d93ccdc62e59e70e7af445467d571
Matéo RABOTIN <mateo@rabotin.net>
2025-04-01 21:34:12 +0200

    feat: html autoclose
    Signed-off-by: Matéo RABOTIN <mateo@rabotin.net>

--- lua/custom/conf/autocmds.lua
+++ lua/custom/conf/autocmds.lua
@@ -1,5 +1,5 @@
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    vim.cmd 'Neotree position=current'
-  end,
-})
+-- vim.api.nvim_create_autocmd('VimEnter', {
+--   callback = function()
+--     vim.cmd 'Neotree position=current'
+--   end,
+-- })
