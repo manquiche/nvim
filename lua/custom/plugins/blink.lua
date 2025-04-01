ca124fd8e5f9c169e9d25ea34fbaa741d15def90
mateo.rabotin <mateo.rabotin@veolia.com>
2025-04-01 21:30:34 +0200

    --wip-- [skip ci]

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -39,30 +39,30 @@ return {
       documentation = {
         auto_show = false,
       },
-      menu = {
-        draw = {
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                return kind_icon
-              end,
-              -- (optional) use highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                return hl
-              end,
-            },
-            kind = {
-              -- (optional) use highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                return hl
-              end,
-            },
-          },
-        },
-      },
+      -- menu = {
+      --   draw = {
+      --     components = {
+      --       kind_icon = {
+      --         text = function(ctx)
+      --           local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+      --           return kind_icon
+      --         end,
+      --         -- (optional) use highlights from mini.icons
+      --         highlight = function(ctx)
+      --           local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+      --           return hl
+      --         end,
+      --       },
+      --       kind = {
+      --         -- (optional) use highlights from mini.icons
+      --         highlight = function(ctx)
+      --           local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+      --           return hl
+      --         end,
+      --       },
+      --     },
+      --   },
+      -- },
     },
 
     -- Default list of enabled providers defined so that you can extend it
---
+++ lua/custom/plugins/yaml-companion.lua
@@ -0,0 +1,58 @@
+--- @type LazySpec
+return {
+  'someone-stole-my-name/yaml-companion.nvim',
+  dependencies = {
+    { 'neovim/nvim-lspconfig' },
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope.nvim' },
+  },
+  config = function(_, opts)
+    require('telescope').load_extension 'yaml_schema'
+    local cfg = require('yaml-companion').setup(opts)
+    require('lspconfig')['yamlls'].setup(cfg)
+  end,
+  opts = {
+    -- Built in file matchers
+    builtin_matchers = {
+      -- Detects Kubernetes files based on content
+      kubernetes = { enabled = true },
+      cloud_init = { enabled = true },
+    },
+
+    -- Additional schemas available in Telescope picker
+    schemas = {
+      {
+        name = 'Kubernetes 1.32.3',
+        uri = 'https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.32.3-standalone-strict/all.json',
+      },
+    },
+
+    -- Pass any additional options that will be merged in the final LSP config
+    lspconfig = {
+      flags = {
+        debounce_text_changes = 150,
+      },
+      settings = {
+        redhat = { telemetry = { enabled = false } },
+        yaml = {
+          validate = true,
+          format = { enable = true },
+          hover = true,
+          -- schemaStore = {
+          --   enable = true,
+          --   url = 'https://www.schemastore.org/api/json/catalog.json',
+          -- },
+          schemaStore = {
+            enable = false,
+            url = '',
+          },
+          -- schemas = {
+          --   kubernetes = { 'k8s**.yaml', 'kube*/*.yaml' },
+          -- },
+          schemaDownload = { enable = true },
+          trace = { server = 'debug' },
+        },
+      },
+    },
+  },
+}
