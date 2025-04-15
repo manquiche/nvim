74464e04a0cff73f3cc0fd774b89b0c6521e95d3
mateo.rabotin <mateo.rabotin@veolia.com>
2025-04-15 16:24:51 +0200

    refactor: fix crappy aider code from test session
    Signed-off-by: mateo.rabotin <mateo.rabotin@veolia.com>

--- lua/custom/conf/keymaps.lua
+++ lua/custom/conf/keymaps.lua
@@ -73,3 +73,22 @@ end, { desc = 'Show test output' })
 vim.keymap.set('n', '<leader>nw', function()
   require('neotest').watch.toggle(vim.fn.expand '%')
 end, { desc = 'Toggle watching file' })
+
+-- Copilot all neovim keymaps
+vim.keymap.set('i', '<M-CR>', function()
+  require('copilot.suggestion').accept()
+end, { desc = 'Accept Copilot suggestion' })
+vim.keymap.set('i', '<M-e>', function()
+  require('copilot.suggestion').dismiss()
+end, { desc = 'Dismiss Copilot suggestion' })
+vim.keymap.set('i', '<M-n>', function()
+  require('copilot.suggestion').next()
+end, { desc = 'Next Copilot suggestion' })
+vim.keymap.set('i', '<M-y>', function()
+  require('copilot.suggestion').prev()
+end, { desc = 'Previous Copilot suggestion' })
+
+--- Enable/disable copilot
+vim.keymap.set('n', '<leader>gc', function()
+  require('copilot').toggle()
+end, { desc = 'Toggle Copilot' })
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,50 +4,48 @@ return {
   cmd = 'Copilot',
   build = ':Copilot auth',
   event = 'BufReadPost',
-  config = function()
-    require('copilot').setup({
-      panel = {
-        enabled = true,
-        auto_refresh = true,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-CR>",
-        },
-        layout = {
-          position = "bottom", -- | top | left | right
-          ratio = 0.4,
-        },
+  opts = {
+    panel = {
+      enabled = true,
+      auto_refresh = true,
+      keymap = {
+        jump_prev = '[[',
+        jump_next = ']]',
+        accept = '<CR>',
+        refresh = 'gr',
+        open = '<M-CR>',
       },
-      suggestion = {
-        enabled = not vim.g.ai_cmp,
-        auto_trigger = true,
-        hide_during_completion = vim.g.ai_cmp,
-        keymap = {
-          accept = "<M-l>",
-          accept_word = "<M-w>",
-          accept_line = "<M-j>",
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
-        debounce = 75,
+      layout = {
+        position = 'bottom', -- | top | left | right
+        ratio = 0.4,
       },
-      filetypes = {
-        markdown = true,
-        help = true,
-        yaml = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
+    },
+    suggestion = {
+      enabled = not vim.g.ai_cmp,
+      auto_trigger = true,
+      hide_during_completion = vim.g.ai_cmp,
+      keymap = {
+        accept = '<M-y>',
+        accept_word = '<M-w>',
+        accept_line = '<M-j>',
+        next = '<M-n>',
+        prev = '<M-p>',
+        dismiss = '<C-]>',
       },
-      copilot_node_command = 'node', -- Node.js version must be > 16.x
-      server_opts_overrides = {},
-    })
-  end,
+      debounce = 75,
+    },
+    filetypes = {
+      markdown = true,
+      help = true,
+      yaml = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ['.'] = false,
+    },
+    copilot_node_command = 'node', -- Node.js version must be > 16.x
+    server_opts_overrides = {},
+  },
 }
